46516ae181709f7c41e76006a5bdfbb9a4e51ea6
ZivKassner <zivkassner@gmail.com>
2024-10-07 15:57:13 +0300

    now all loads config

--- src/inference/inference_pipeline.py
+++ src/inference/inference_pipeline.py
@@ -4,33 +4,30 @@ from src.utils.config import load_config
 from src.inference.inferer import Inferer
 from src.postprocess.saver import ResultsParser
 
+
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Run YOLO inference with tracking or detection.")
-    parser.add_argument('--config', type=str, default="config.json", help='Path to the JSON configuration file.')
+    parser = argparse.ArgumentParser(
+        description="Run YOLO inference with tracking or detection."
+    )
+    parser.add_argument(
+        "--config_path",
+        type=str,
+        default="config.json",
+        help="Path to the JSON configuration file.",
+    )
     return parser.parse_args()
+
 
 def main() -> None:
     args = parse_args()
-    
-    config: Dict[str, Any] = load_config(args.config)
-
-    inferer = Inferer(
-        model_path=config["model"].get("weights"),
-        task=config["model"].get("task"),             
-        output_dir=config["output"].get("output_dir"),
-        images_dir=config["input"].get("images_dir"),         
-        save_animations=config["output"].get("save_animations") 
-    )
-    
+    config = load_config(args.config_path)
+    inferer = Inferer(config=config)
     results = inferer.infer()
 
-    parser = ResultsParser(
-        results=results, 
-        output_dir=inferer.output_dir, 
-        task=config["model"].get("task")
-    )
-    
-    parser.parse_and_save()  
+    parser = ResultsParser(results=results, config=config)
+
+    parser.parse_and_save()
+
 
 if __name__ == "__main__":
     main()
