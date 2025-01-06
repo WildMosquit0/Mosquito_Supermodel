a176525245ea2b46578f780c1106939cc382cbfe
WildMosquit0 <evyatar.sar-shalom@mail.huji.ac.il>
2025-01-07 01:15:41 +0200

    updated main

--- main.py
+++ main.py
@@ -9,21 +9,20 @@ if __name__ == "__main__":
     config_path = "./config.json"
 
     # ! Step 1: Run the following line to predict/track
-    #run_inference(config_path=config_path)
+    run_inference(config_path=config_path)
     
     # ! Step 2: Run the following line to explore and analyze the model's trajectories
     define_roi_main = ROIDefiner(config_path)
+    define_roi_main()
     explorer = PlotXY(config_path)
     average_visits = AverageVisits(config_path)
     #duration = Duration(config_path)
     
     
-    #explorer()
-    #average_visits()
-    #duration()
-    define_roi_main()
     
-    # ** to define ROI for video, uncomment the following line **
-    #! remember to adjust the 'csv path' in the config file to /path/to/your/roi_results.csv
+    explorer()
+    average_visits()
+    #duration()
+    
     
     
