1d16ab3acf4a851a43bc49f26be3afcaf70136c0
Wildmosquit0 <evyatar.sar-shalom@mail.huji.ac.il>
2025-01-14 15:43:35 +0200

    plot_taning_process

--- src/postprocess/use_sahi.py
+++ src/postprocess/use_sahi.py
@@ -1,19 +1,37 @@
 from sahi.predict import predict as sahi_predict
 from ultralytics import YOLO
-
+import os
 # SAHI Prediction with your specific arguments
 
 model = "/home/bohbot/Evyatar/runs/detect/most_update_batch3/weights/best.pt"
-source = "/home/bohbot/Evyatar/runs/IMG_7631_frame6576.png"
+source = "/home/bohbot/Evyatar/runs/small"
 
+conf = 0.4
+
+
+#from sahi.slicing import slice_image
+#for i in os.listdir(source):
+#    img_name = os.path.join(source,i)
+#    slice_image_result = slice_image(
+#    image=img_name,
+#    output_file_name= os.path.basename(i),
+#    output_dir="run",
+#    slice_height=640,
+#    slice_width=640,
+#    overlap_height_ratio=0.2,
+#    overlap_width_ratio=0.2,
+#    )
+#
+
+new_source = "run"
 sahi_predict(
     model_type="ultralytics",  # Use the model type specific to your YOLO version
     model_path= model,
-    model_device="cpu",  # Set to 'cuda:0' if using GPU
-    model_confidence_threshold=0.1,
+    model_device="cuda:0",  # Set to 'cuda:0' if using GPU
+    model_confidence_threshold=conf,
     source=source,  # Path to directory containing images
-    slice_height=256,
-    slice_width=256,
+    slice_height=640,
+    slice_width=640,
     overlap_height_ratio=0.2,
     overlap_width_ratio=0.2
 )
@@ -21,11 +39,10 @@ sahi_predict(
 
 # Load the YOLO model
 model = YOLO(model)
-
 # Perform prediction
 results = model.predict(
     source=source,
-    conf=0.4,  # Confidence threshold for predictions
+    conf=conf,  # Confidence threshold for predictions
     save=True,
     imgsz=640  # Save the results
 )
