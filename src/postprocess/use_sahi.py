8d52ed2cf2ae9423ff021f9779c6f458677c6be2
WildMosquit0 <evyatar.sar-shalom@mail.huji.ac.il>
2025-01-29 15:30:24 +0200

    added croppig movies

--- src/postprocess/use_sahi.py
+++ src/postprocess/use_sahi.py
@@ -3,8 +3,8 @@ from ultralytics import YOLO
 import os
 # SAHI Prediction with your specific arguments
 
-model = "/home/bohbot/Evyatar/runs/detect/sahi_1_l/weights/best.pt"
-source = "/home/bohbot/Evyatar/runs/sugar.avi"
+model = "/home/wildmosquit0/git/bestModel/test_different_models/sahi_l_1_more_300e_200.pt"
+source = "/home/wildmosquit0/git/model_exp/peers/2023-07-07_10-47-35_cropped_m.mp4"
 
 conf = 0.25
 
@@ -23,26 +23,29 @@ conf = 0.25
 #    )
 #
 
-new_source = "run"
-sahi_predict(
-    model_type="ultralytics",  # Use the model type specific to your YOLO version
-    model_path= model,
-    model_device="cuda:0",  # Set to 'cuda:0' if using GPU
-    model_confidence_threshold=conf,
-    source=source,  # Path to directory containing images
-    slice_height=640,
-    slice_width=640,
-    overlap_height_ratio=0.2,
-    overlap_width_ratio=0.2
-)
+#new_source = "run"
+#sahi_predict(
+#    model_type="ultralytics",  # Use the model type specific to your YOLO version
+#    model_path= model,
+#    model_device="cuda:0",  # Set to 'cuda:0' if using GPU
+#    model_confidence_threshold=conf,
+#    source=source,  # Path to directory containing images
+#    slice_height=640,
+#    slice_width=640,
+#    overlap_height_ratio=0.2,
+#    overlap_width_ratio=0.2
+#)
+#
 
-
-# Load the YOLO model
+## Load the YOLO model
 model = YOLO(model)
 # Perform prediction
 results = model.predict(
     source=source,
     conf=conf,  # Confidence threshold for predictions
     save=True,
-    imgsz=640  # Save the results
+    imgsz=640,  # Save the results
+    line_width = 1,
+    show_labels = False,
+    show_conf = False
 )
