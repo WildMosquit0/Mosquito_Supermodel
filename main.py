5ce3733b7ff441cd98b93cdf9cb8bb95cbe1863c
Wildmosquit0 <evyatar.sar-shalom@mail.huji.ac.il>
2025-06-26 12:49:37 +0300

    update

--- main.py
+++ main.py
@@ -7,26 +7,26 @@ from src.inference.inference_pipeline import run_inference
 from src.analyze.analysis_pipeline import run_analysis  # Ensure you have this implemented
 from src.utils.config import load_config
 
-def main(task_name: str) -> None:
+def main(task: str) -> None:
 
-    conf_yaml_path =  os.path.abspath(f"configs/{task_name}.yaml")
+    conf_yaml_path =  os.path.abspath(f"configs/{task}.yaml")
     config = load_config(conf_yaml_path)
     
-    if task_name == 'infer':
+    if task == 'infer':
         run_inference(config)
-    elif task_name == 'analyze':
+    elif task == 'analyze':
         run_analysis(config,conf_yaml_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run YOLO inference with tracking, detection, or slicing.')
-    parser.add_argument('--task_name', choices=['infer', 'analyze'], default='infer',  help='The task to be performed')
+    parser.add_argument('--task', choices=['infer', 'analyze'], default='infer',  help='The task to be performed')
     args = parser.parse_args()
 
     profiler = cProfile.Profile()
     profiler.enable()
     
     try:
-        main(args.task_name)
+        main(args.task)
     finally:
         profiler.disable()
         stats = pstats.Stats(profiler)
